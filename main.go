734dfac7f4f12a66c45d64bcec087804275faeae
Cyrille M <cmeichel@free.fr>
2016-09-04 09:29:52 +0200

    reorder comments

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "path/filepath"
-    "regexp"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
 
-    "github.com/landru29/swaggo/parser"
+	"github.com/landru29/swaggo/parser"
 )
 
 /* Gros comments
@@ -14,27 +14,28 @@ sur deux lignes */
 
 // ceci est un test
 func getFileList(searchDir string) (fileList []string, err error) {
-    fileList = []string{}
-    var goFileRegExp = regexp.MustCompile(`\.go$`)
-    var notHiddentDirRegExp = regexp.MustCompile(`\/\.\w+|^\.\w+`)
-    err = filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
-        if !f.IsDir() && goFileRegExp.MatchString(f.Name()) && !notHiddentDirRegExp.MatchString(path) {
-            fileList = append(fileList, path)
-        }
-        return nil
-    })
-    return
+	fileList = []string{}
+	var goFileRegExp = regexp.MustCompile(`\.go$`)
+	var notHiddentDirRegExp = regexp.MustCompile(`\/\.\w+|^\.\w+`)
+	err = filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() && goFileRegExp.MatchString(f.Name()) && !notHiddentDirRegExp.MatchString(path) {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	return
 }
 
 // et un autre
 func main() {
-    filenames, err := getFileList(".")
-    if err == nil {
-        for _, filename := range filenames {
-            comments, _ := parser.ParseComments(filename)
-            for _, comments := range comments {
-                fmt.Println(comments)
-            }
-        }
-    }
+	filenames, err := getFileList(".")
+	if err == nil {
+		for _, filename := range filenames {
+			fmt.Printf("############ %s ############\n", filename)
+			comments, _ := parser.ParseComments(filename)
+			for _, comments := range comments {
+				fmt.Println(comments)
+			}
+		}
+	}
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,36 +1,65 @@
 package parser
 
 import (
-    "io/ioutil"
-    "regexp"
-    "strings"
+	"io/ioutil"
+	"regexp"
+	"sort"
+	"strings"
 )
 
+type commentStruct struct {
+	Value string
+	Pos   int
+}
+
+type byIndex []commentStruct
+
+func (a byIndex) Len() int {
+	return len(a)
+}
+
+func (a byIndex) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a byIndex) Less(i, j int) bool {
+	return a[i].Pos < a[j].Pos
+}
+
 // ParseComments parse all the comments
-func ParseComments(filename string) (comments []string, err error) {
-    comments = []string{}
-    dat, err := ioutil.ReadFile(filename)
-    contentFile := string(dat)
+func ParseComments(filename string) (commentStr []string, err error) {
+	comments := []commentStruct{}
+	commentStr = []string{}
+	dat, err := ioutil.ReadFile(filename)
+	contentFile := string(dat)
 
-    // Inline comments
-    inlineCommentRegExp := regexp.MustCompile(`\/\/\s?(.*)`)
-    inlineComment := inlineCommentRegExp.FindAllString(contentFile, -1)
-    for _, comment := range inlineComment {
-        filtered := inlineCommentRegExp.FindStringSubmatch(comment)
-        comments = append(comments, filtered[1])
-    }
+	// Inline comments
+	inlinecommentStructRegExp := regexp.MustCompile(`\/\/\s?(.*)`)
+	inlinecommentStruct := inlinecommentStructRegExp.FindAllString(contentFile, -1)
+	inlinecommentStructIndex := inlinecommentStructRegExp.FindAllStringIndex(contentFile, -1)
+	for key, comment := range inlinecommentStruct {
+		filtered := inlinecommentStructRegExp.FindStringSubmatch(comment)
+		comments = append(comments, commentStruct{filtered[1], inlinecommentStructIndex[key][0]})
+	}
 
-    // block comments
-    blockCommentRegExp := regexp.MustCompile(`\/\*(\*([^\/])|[^*])*\*\/`)
-    blockComment := blockCommentRegExp.FindAllStringSubmatch(contentFile, -1)
-    for _, comment := range blockComment {
-        commentStr := strings.Replace(comment[0], "/"+"*", "", -1)
-        commentStr = strings.Replace(commentStr, "*"+"/", "", -1)
-        lines := strings.Split(commentStr, "\n")
-        for _, line := range lines {
-            comments = append(comments, line)
-        }
-    }
+	// block comments
+	blockcommentStructRegExp := regexp.MustCompile(`\/\*(\*([^\/])|[^*])*\*\/`)
+	blockcommentStruct := blockcommentStructRegExp.FindAllStringSubmatch(contentFile, -1)
+	blockcommentStructIndex := blockcommentStructRegExp.FindAllStringIndex(contentFile, -1)
+	for key, comment := range blockcommentStruct {
+		commentStr := strings.Replace(comment[0], "/"+"*", "", -1)
+		commentStr = strings.Replace(commentStr, "*"+"/", "", -1)
+		lines := strings.Split(commentStr, "\n")
+		for index, line := range lines {
+			comments = append(comments, commentStruct{line, blockcommentStructIndex[key][0] + index})
+		}
+	}
 
-    return
+	// compile all
+	sort.Sort(byIndex(comments))
+	for _, comment := range comments {
+		commentStr = append(commentStr, comment.Value)
+	}
+
+	return
 }
