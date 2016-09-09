d027d5099623d94b2671bc1fe2f05d06497f14e2
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2016-09-09 14:15:06 +0200

    enrich swagger structures

--- swagger/create.go
+++ swagger/create.go
@@ -9,7 +9,7 @@ import (
 // NewSwagger create a new swagger object
 func NewSwagger() Swagger {
     return Swagger{
-        Scheme:   strings.Split(viper.GetString("api_scheme"), ","),
+        Schemes:  strings.Split(viper.GetString("api_scheme"), ","),
         Swagger:  "2.0",
         Host:     viper.GetString("api_host"),
         BasePath: viper.GetString("api_basepath"),
--- swagger/structure.go
+++ swagger/structure.go
@@ -1,48 +1,97 @@
 package swagger
 
+// http://swagger.io/specification
+
 //Swagger is the swagger structure
 type Swagger struct {
-    Consumes []string `json:"consumes"`
-    Produces []string `json:"produces"`
-    Scheme   []string `json:"scheme"`
-    Swagger  string   `json:"swagger"`
-    Host     string   `json:"host"`
-    BasePath string   `json:"basePath"`
-    Info     Info     `json:"info"`
-    Paths    Path     `json:"paths"`
+    Consumes            []string                  `json:"consumes"`
+    Produces            []string                  `json:"produces"`
+    Schemes             []string                  `json:"schemes"`
+    Swagger             string                    `json:"swagger"`
+    Host                string                    `json:"host"`
+    BasePath            string                    `json:"basePath"`
+    Info                InfoStruct                `json:"info"`
+    Paths               PathsStruct               `json:"paths"`
+    Definitions         DefinitionsStruct         `json:"definitions"`
+    Parameters          ParametersStruct          `json:"parameters"`
+    Responses           ResponsesStruct           `json:"responses"`
+    Tags                []TagStruct               `json:"tags"`
+    ExternalDocs        ExternalDocsStruct        `json:"externalDocs"`
+    SecurityDefinitions SecurityDefinitionsStruct `json:"securityDefinitions"`
+    Security            []SecurityStruct          `json:"security"`
 }
 
-//Info is the swagger info structure
-type Info struct {
-    Description    string  `json:"description"`
-    Title          string  `json:"title"`
-    Version        string  `json:"version"`
-    TermsOfService string  `json:"termsOfService"`
-    License        License `json:"licence"`
-    Contact        Contact `json:"contact"`
+//InfoStruct is the swagger info structure
+type InfoStruct struct {
+    Description    string        `json:"description"`
+    Title          string        `json:"title"`
+    Version        string        `json:"version"`
+    TermsOfService string        `json:"termsOfService"`
+    License        LicenseStruct `json:"licence"`
+    Contact        ContactStruct `json:"contact"`
 }
 
-//License is the swagger licence structure
-type License struct {
+// DefinitionsStruct is the swagger definition structure
+type DefinitionsStruct struct {
+}
+
+// ParametersStruct is the swagger parameter structure
+type ParametersStruct struct {
+}
+
+// ResponsesStruct is the swagger response structure
+type ResponsesStruct struct {
+}
+
+// ExternalDocsStruct is the swagger external doc structure
+type ExternalDocsStruct struct {
+}
+
+// SecurityDefinitionsStruct is the swagger security definition structure
+type SecurityDefinitionsStruct struct {
+}
+
+// SecurityStruct is the swagger security structure
+type SecurityStruct struct {
+}
+
+// TagStruct is the swagger tag structure
+type TagStruct struct {
+}
+
+//LicenseStruct is the swagger licence structure
+type LicenseStruct struct {
     Name string `json:"Name"`
     URL  string `json:"url"`
 }
 
-//Contact is the swagger contact structure
-type Contact struct {
+//ContactStruct is the swagger contact structure
+type ContactStruct struct {
     Email string `json:"email"`
 }
 
-// RouteDef is the swagger route definition
-type RouteDef struct {
-    Description string   `json:"description"`
-    Consumes    []string `json:"consumes"`
-    Produces    []string `json:"produces"`
-    Tags        []string `json:"tags"`
+// ParameterStruct is the swagger parameter structure
+type ParameterStruct struct {
 }
 
-// Route is the swagger routes list (get => ..., post => ...)
-type Route map[string]RouteDef
+// OperationStruct is the swagger route definition
+type OperationStruct struct {
+    Tags         []string           `json:"tags"`
+    Summary      []string           `json:"summary"`
+    Description  string             `json:"description"`
+    ExternalDocs ExternalDocsStruct `json:"externalDocs"`
+    OperationID  string             `json:"operationId"`
+    Consumes     []string           `json:"consumes"`
+    Produces     []string           `json:"produces"`
+    Parameters   []ParameterStruct  `json:"parameters"`
+    Responses    ResponsesStruct    `json:"responses"`
+    Schemes      []string           `json:"schemes"`
+    Deprecated   bool               `json:"deprecated"`
+    Security     []SecurityStruct   `json:"security"`
+}
 
-// Path is the swagget path (path => Route)
-type Path map[string]Route
+// PathItemStruct is the swagger routes list (get => ..., post => ...)
+type PathItemStruct map[string]OperationStruct
+
+// PathsStruct is the swagget path (path => Route)
+type PathsStruct map[string]PathItemStruct
